84ad82f708a9b008c275a9d48a9ec00e34dccb1a
syarafis <syarafis@gmail.com>
2019-11-20 15:02:34 +0800

    Updates

---
+++ precache-manifest.5171c5f4fd689476a545958ed1cd0a32.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34ebee77f5a0115bbacb5d5a4fea539",
+    "url": "/user-app/index.html"
+  },
+  {
+    "revision": "674e12378844294727f8",
+    "url": "/user-app/static/js/2.711ee3a3.chunk.js"
+  },
+  {
+    "revision": "009e68e95b094bacec67",
+    "url": "/user-app/static/js/main.3ef057fa.chunk.js"
+  },
+  {
+    "revision": "809e773395835cc2a0a3",
+    "url": "/user-app/static/js/runtime-main.329ee413.js"
+  }
+]);
